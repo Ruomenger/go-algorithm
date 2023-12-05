ae0a17ce7f316b4f93a3cb71cdeb4b8bd826440f
ruomeng0x <ruomenger@foxmail.com>
2023-12-05 23:22:18 +0800

    temp: 0004 solutions

--- solutions/p0004/p0004.go
+++ solutions/p0004/p0004.go
@@ -1,5 +1,48 @@
 package p0101
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0
+	totalLen := len(nums1) + len(nums2)
+	if totalLen == 0 {
+		return 0
+	}
+	len1 := len(nums1)
+	len2 := len(nums2)
+	two := totalLen%2 == 0
+
+	left1 := 0
+	right1 := len1 - 1
+	left2 := 0
+	right2 := len2 - 1
+	i := left1
+	j := left2
+	// 0 1   2   3
+	// 1 4   8i
+	// 2 10j 20  24
+	for i+j+1 < totalLen/2 {
+		if i < len1 && j < len2 {
+			if nums1[i] < nums2[j] {
+				left1 = i
+				i = (i+right1)/2 + 1
+			} else {
+				left2 = j
+				j = (j+right2)/2 + 1
+			}
+		} else if i == len1 {
+			if nums1[i] < nums2[j] {
+				right2 = j
+				j = (left2+j)/2 + 1
+			} else {
+				left2 = j
+				j = (j+right2)/2 + 1
+			}
+		}
+
+	}
+	if !two {
+		if nums1[i] > nums2[j] {
+			return float64(nums2[i])
+		}
+		return float64(nums1[j])
+	}
+	return float64(nums1[i]+nums2[j]) / 2.0
 }
--- solutions/p0004/p0004_test.go
+++ solutions/p0004/p0004_test.go
@@ -1,6 +1,7 @@
 package p0101
 
 import (
+	"math"
 	"testing"
 )
 
@@ -10,24 +11,30 @@ func TestIsUnique(t *testing.T) {
 		nums1, nums2 []int
 		expected     float64
 	}{
-		{
-			description: "example1",
-			nums1:       []int{1, 3},
-			nums2:       []int{2},
-			expected:    2.0,
-		},
+		//{
+		//	description: "example1",
+		//	nums1:       []int{1, 3},
+		//	nums2:       []int{2},
+		//	expected:    2.0,
+		//},
+		//{
+		//	description: "example2",
+		//	nums1:       []int{1, 2},
+		//	nums2:       []int{3, 4},
+		//	expected:    2.5,
+		//},
 		{
 			description: "example2",
-			nums1:       []int{1, 2},
-			nums2:       []int{3, 4},
-			expected:    2.5,
+			nums1:       []int{1, 2, 10, 12},
+			nums2:       []int{3, 6, 100},
+			expected:    6.0,
 		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.description, func(t *testing.T) {
 			result := findMedianSortedArrays(tt.nums1, tt.nums2)
-			if result != tt.expected {
+			if math.Abs(result-tt.expected) > 1e-9 {
 				t.Errorf("expected %f, but got %f", tt.expected, result)
 			}
 		})
