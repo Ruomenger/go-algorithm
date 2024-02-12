a620f03a98c005f0c06b85cb35c8b91045490250
meng ruo <Ruomenger>
2024-02-12 13:43:26 +0800

    add: 新增 203. 移除链表元素

---
+++ category/list/list_test.go
@@ -0,0 +1,27 @@
+package list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveElements(t *testing.T) {
+	type args struct {
+		head *ListNode
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{name: "example0", args: args{head: nil, val: 0}, want: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeElements(tt.args.head, tt.args.val); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
