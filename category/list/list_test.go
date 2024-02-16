cc327928f5dfc66a29ebe207bf90170e2e923896
meng ruo <Ruomenger>
2024-02-16 10:46:17 +0800

    add: 新增 24. 两两交换链表中的节点

--- category/list/list_test.go
+++ category/list/list_test.go
@@ -25,3 +25,43 @@ func TestRemoveElements(t *testing.T) {
 		})
 	}
 }
+
+func Test_swapPairs(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+
+	head1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	head2 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	head3 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	head4 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	head1.Next = head2
+	head2.Next = head3
+	head3.Next = head4
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{name: "example0", args: args{head: head1}, want: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := swapPairs(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("swapPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
