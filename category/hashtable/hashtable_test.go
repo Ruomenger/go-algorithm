b5aab6db548e7427dcfbd77fc70cfa7745596e93
ruomeng0x <ruomenger@foxmail.com>
2024-02-17 23:39:20 +0800

    add: 新增 15. 三数之和

--- category/hashtable/hashtable_test.go
+++ category/hashtable/hashtable_test.go
@@ -160,3 +160,24 @@ func TestCanConstruct(t *testing.T) {
 		})
 	}
 }
+
+func TestThreeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"example1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
+		{"example2", args{[]int{0, 1, 1}}, [][]int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
