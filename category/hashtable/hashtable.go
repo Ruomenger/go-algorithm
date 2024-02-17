acada5fd94921fe47800530b1b8e3cf5e28e97a9
ruomeng0x <ruomenger@foxmail.com>
2024-02-17 20:58:58 +0800

    add: 新增 1002. 查找共用字符

--- category/hashtable/hashtable.go
+++ category/hashtable/hashtable.go
@@ -17,3 +17,33 @@ func isAnagram(s string, t string) bool {
 	}
 	return true
 }
+
+// commonChars 1002. 查找共用字符 https://leetcode.cn/problems/find-common-characters/
+func commonChars(words []string) []string {
+	myMin := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+	var count [26]int
+	for i := range count {
+		count[i] = 10000
+	}
+	ans := make([]string, 0)
+	for _, word := range words {
+		cur := [26]int{}
+		for i := range word {
+			cur[word[i]-'a']++
+		}
+		for i := range cur {
+			count[i] = myMin(count[i], cur[i])
+		}
+	}
+	for i := range count {
+		for j := 0; j < count[i]; j++ {
+			ans = append(ans, string(rune('a'+i)))
+		}
+	}
+	return ans
+}
--- category/hashtable/hashtable_test.go
+++ category/hashtable/hashtable_test.go
@@ -1,6 +1,9 @@
 package hashtable
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIsAnagram(t *testing.T) {
 	type args struct {
@@ -19,6 +22,27 @@ func TestIsAnagram(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isAnagram(tt.args.s, tt.args.t); got != tt.want {
 				t.Errorf("isAnagram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommonChars(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"example1", args{[]string{"bella", "label", "roller"}}, []string{"e", "l", "l"}},
+		{"example2", args{[]string{"cool", "lock", "cook"}}, []string{"c", "o"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := commonChars(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("commonChars() = %v, want %v", got, tt.want)
 			}
 		})
 	}
