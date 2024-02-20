0271f282f4b2af05031260c9bf66c244578ca833
ruomeng0x <ruomenger@foxmail.com>
2024-02-21 00:08:33 +0800

    add: 新增 栈与队列 225. 用队列实现栈

--- category/stack-queue/stack-queue.go
+++ category/stack-queue/stack-queue.go
@@ -72,3 +72,46 @@ func (q *MyQueue) Empty() bool {
  * param_3 := obj.Peek();
  * param_4 := obj.Empty();
  */
+
+type MyStack struct {
+	queue []int
+}
+
+func Constructor1() MyStack {
+	return MyStack{
+		queue: make([]int, 0),
+	}
+}
+
+func (s *MyStack) Push(x int) {
+	n := len(s.queue)
+	s.queue = append(s.queue, x)
+	for ; n > 0; n-- {
+		s.queue = append(s.queue, s.queue[0])
+		s.queue = s.queue[1:]
+	}
+
+}
+
+func (s *MyStack) Pop() int {
+	v := s.queue[0]
+	s.queue = s.queue[1:]
+	return v
+}
+
+func (s *MyStack) Top() int {
+	return s.queue[0]
+}
+
+func (s *MyStack) Empty() bool {
+	return len(s.queue) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
