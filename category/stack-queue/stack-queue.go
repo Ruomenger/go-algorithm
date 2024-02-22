5c769307ab19684009d7c849e870daf56daf62a1
ruomeng0x <ruomenger@foxmail.com>
2024-02-22 22:36:20 +0800

    add 新增 栈与队列 347. 前 K 个高频元素

--- category/stack-queue/stack-queue.go
+++ category/stack-queue/stack-queue.go
@@ -1,6 +1,9 @@
 package stack_queue
 
-import "strconv"
+import (
+	"container/heap"
+	"strconv"
+)
 
 type Stack []int
 
@@ -240,6 +243,53 @@ func maxSlidingWindow(nums []int, k int) []int {
 		q.Pop(nums[i-k])
 		q.Push(nums[i])
 		res = append(res, q.Front())
+	}
+	return res
+}
+
+type IntHeap [][2]int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.([2]int))
+}
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// topKFrequent 347. 前 K 个高频元素 https://leetcode.cn/problems/top-k-frequent-elements/
+func topKFrequent(nums []int, k int) []int {
+	numMap := map[int]int{}
+	for _, num := range nums {
+		numMap[num]++
+	}
+	h := &IntHeap{}
+	heap.Init(h)
+	//所有元素入堆，堆的长度为k
+	for key, value := range numMap {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	res := make([]int, k)
+	for i := 0; i < k; i++ {
+		res[k-i-1] = heap.Pop(h).([2]int)[0]
 	}
 	return res
 }
