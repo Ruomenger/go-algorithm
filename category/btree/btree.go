5facf56c926d767ff9a3c2049a3c7614463eaa60
ruomeng0x <ruomenger@foxmail.com>
2024-02-26 00:05:11 +0800

    add: 新增 二叉树 145. 二叉树的后序遍历

--- category/btree/btree.go
+++ category/btree/btree.go
@@ -82,3 +82,43 @@ func inorderTraversalIter(root *TreeNode) []int {
 	}
 	return nums
 }
+
+// postorderTraversal 递归版 145. 二叉树的后序遍历 https://leetcode.cn/problems/binary-tree-postorder-traversal
+func postorderTraversalRec(root *TreeNode) []int {
+	nums := make([]int, 0)
+	var postorder func(node *TreeNode)
+	postorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		postorder(node.Left)
+		postorder(node.Right)
+		nums = append(nums, node.Val)
+	}
+	postorder(root)
+	return nums
+}
+
+// postorderTraversalIter 迭代版 145. 二叉树的后序遍历 https://leetcode.cn/problems/binary-tree-postorder-traversal
+func postorderTraversalIter(root *TreeNode) []int {
+	nums := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	var prev *TreeNode
+	for len(stack) != 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Right == nil || root.Right == prev {
+			nums = append(nums, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stack = append(stack, root)
+			root = root.Right
+		}
+	}
+	return nums
+}
--- category/btree/btree_test.go
+++ category/btree/btree_test.go
@@ -100,3 +100,51 @@ func TestInorderTraversal(t *testing.T) {
 		})
 	}
 }
+
+func TestPostorderTraversalRec(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	test1 := args{
+		&TreeNode{
+			Val:   1,
+			Left:  nil,
+			Right: nil,
+		}}
+	test1.root.Right = &TreeNode{Val: 2}
+	test1.root.Right.Right = &TreeNode{Val: 3}
+	test2 := args{}
+	test3 := args{
+		&TreeNode{
+			Val:   1,
+			Left:  nil,
+			Right: nil,
+		}}
+	test4 := args{
+		&TreeNode{
+			Val: 3,
+		},
+	}
+	test4.root.Left = &TreeNode{Val: 1}
+	test4.root.Right = &TreeNode{Val: 2}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test1", test1, []int{3, 2, 1}},
+		{"test2", test2, []int{}},
+		{"test3", test3, []int{1}},
+		{"test4", test4, []int{1, 2, 3}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := postorderTraversalRec(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("postorderTraversalRec() = %v, want %v", got, tt.want)
+			}
+			if got := postorderTraversalIter(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("postorderTraversalRec() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
