9faf79681720a4b4b983f4331a75a376cc32829b
ruomeng0x <ruomenger@foxmail.com>
2024-11-03 16:37:30 +0800

    add: 回溯 51. N 皇后 meduim

--- category/backtracking/backtracking_test.go
+++ category/backtracking/backtracking_test.go
@@ -267,3 +267,24 @@ func TestPermuteUnique(t *testing.T) {
 		})
 	}
 }
+
+func Test_solveNQueens(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{"test1", args{1}, [][]string{{"Q"}}},
+		{"test1", args{4}, [][]string{{".Q..", "...Q", "Q...", "..Q."}, {"..Q.", "Q...", "...Q", ".Q.."}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveNQueens(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("solveNQueens() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
