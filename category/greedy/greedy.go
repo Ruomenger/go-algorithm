db6177c82f05dc00c7ac1f23141a7928dc9b163d
ruomeng0x <ruomenger@foxmail.com>
2024-11-04 23:50:06 +0800

    add: 贪心 134. 加油站 medium

--- category/greedy/greedy.go
+++ category/greedy/greedy.go
@@ -122,3 +122,20 @@ func largestSumAfterKNegations(nums []int, k int) int {
 	}
 	return sum
 }
+
+// canCompleteCircuit 134. 加油站 https://leetcode.cn/problems/gas-station/
+func canCompleteCircuit(gas []int, cost []int) int {
+	cur, total, start := 0, 0, 0
+	for i := 0; i < len(gas); i++ {
+		cur += (gas[i] - cost[i])
+		total += (gas[i] - cost[i])
+		if cur < 0 {
+			start = i + 1
+			cur = 0
+		}
+	}
+	if total < 0 {
+		return -1
+	}
+	return start
+}
--- category/greedy/greedy_test.go
+++ category/greedy/greedy_test.go
@@ -158,3 +158,25 @@ func Test_largestSumAfterKNegations(t *testing.T) {
 		})
 	}
 }
+
+func Test_canCompleteCircuit(t *testing.T) {
+	type args struct {
+		gas  []int
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{[]int{1, 2, 3, 4, 5}, []int{3, 4, 5, 1, 2}}, 3},
+		{"test1", args{[]int{2, 3, 4}, []int{3, 4, 3}}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canCompleteCircuit(tt.args.gas, tt.args.cost); got != tt.want {
+				t.Errorf("canCompleteCircuit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
