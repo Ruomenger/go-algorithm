ed49768ac310740bb1a6892f50626dac0cbb4e06
ruomeng0x <ruomenger@foxmail.com>
2024-11-07 00:13:13 +0800

    add: 动态规划 96. 不同的二叉搜索树 medium

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -110,3 +110,17 @@ func integerBreak(n int) int {
 	}
 	return dp[n]
 }
+
+// numTrees 96. 不同的二叉搜索树 https://leetcode.cn/problems/unique-binary-search-trees/description/
+func numTrees(n int) int {
+	dp := make([]int, n+2)
+	dp[0] = 1
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] = dp[i] + (dp[j-1] * dp[i-j])
+		}
+	}
+	return dp[n]
+}
--- category/dp/dp_test.go
+++ category/dp/dp_test.go
@@ -136,3 +136,25 @@ func Test_integerBreak(t *testing.T) {
 		})
 	}
 }
+
+func Test_numTrees(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{3}, 5},
+		{"test2", args{1}, 1},
+		{"test3", args{4}, 14},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numTrees(tt.args.n); got != tt.want {
+				t.Errorf("numTrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
