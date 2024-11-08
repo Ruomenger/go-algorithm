79897645526f6996d433f799a133e0a3a9ce0fe4
ruomeng0x <ruomenger@foxmail.com>
2024-11-08 15:53:51 +0800

    add: 动态规划 322. 零钱兑换 medium

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -280,3 +280,23 @@ func combinationSum4(nums []int, target int) int {
 	}
 	return dp[target]
 }
+
+// coinChange 322. 零钱兑换 https://leetcode.cn/problems/coin-change/description/
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = math.MaxInt
+	}
+
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			if dp[j-coins[i]] != math.MaxInt {
+				dp[j] = min(dp[j], dp[j-coins[i]]+1)
+			}
+		}
+	}
+	if dp[amount] == math.MaxInt {
+		return -1
+	}
+	return dp[amount]
+}
--- category/dp/dp_test.go
+++ category/dp/dp_test.go
@@ -291,3 +291,26 @@ func Test_combinationSum4(t *testing.T) {
 		})
 	}
 }
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{[]int{1, 2, 5}, 11}, 3},
+		{"test2", args{[]int{2}, 3}, -1},
+		{"test3", args{[]int{1}, 0}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
