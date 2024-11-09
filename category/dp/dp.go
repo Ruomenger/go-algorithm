a78b1b1347ba501fd8839a46a0b337820a4c28d7
ruomeng0x <ruomenger@foxmail.com>
2024-11-09 12:54:42 +0800

    add: 动态规划 123. 买卖股票的最佳时机 III hard

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -418,3 +418,24 @@ func maxProfit2(prices []int) int {
 	}
 	return dp[len(prices)-1][1]
 }
+
+// maxProfit3 123. 买卖股票的最佳时机 III https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/description/
+func maxProfit3(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([]int, 5)
+	}
+	dp[0][1] = -prices[0]
+	dp[0][3] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = dp[i-1][0]
+		dp[i][1] = max(dp[i-1][1], dp[i][0]-prices[i])
+		dp[i][2] = max(dp[i-1][2], dp[i-1][1]+prices[i])
+		dp[i][3] = max(dp[i-1][3], dp[i-1][2]-prices[i])
+		dp[i][4] = max(dp[i-1][4], dp[i-1][3]+prices[i])
+	}
+	return dp[len(prices)-1][4]
+}
--- category/dp/dp_test.go
+++ category/dp/dp_test.go
@@ -477,3 +477,26 @@ func Test_maxProfit2(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxProfit3(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{[]int{3, 3, 5, 0, 0, 3, 1, 4}}, 6},
+		{"test2", args{[]int{1, 2, 3, 4, 5}}, 4},
+		{"test3", args{[]int{7, 6, 4, 3, 1}}, 0},
+		{"test4", args{[]int{1}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit3(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
