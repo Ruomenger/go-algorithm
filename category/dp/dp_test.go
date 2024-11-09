517180b1647591676bc758d8ba46903bf9f2a272
ruomeng0x <ruomenger@foxmail.com>
2024-11-09 20:33:41 +0800

    add: 动态规划 72. 编辑距离 medium

--- category/dp/dp_test.go
+++ category/dp/dp_test.go
@@ -764,3 +764,25 @@ func Test_minDistance(t *testing.T) {
 		})
 	}
 }
+
+func Test_minDistance2(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{"horse", "ros"}, 3},
+		{"test2", args{"intention", "execution"}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minDistance2(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("minDistance2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
