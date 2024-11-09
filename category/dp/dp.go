b92bb27d29396c2dbc76d66558f96a3cd90cdff5
ruomeng0x <ruomenger@foxmail.com>
2024-11-09 20:08:52 +0800

    add: 动态规划 115. 不同的子序列 medium

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -627,3 +627,32 @@ func isSubsequence(s string, t string) bool {
 	}
 	return i == len(s)
 }
+
+// numDistinct 115. 不同的子序列 https://leetcode.cn/problems/distinct-subsequences/description/
+func numDistinct(s string, t string) int {
+	len1 := len(s)
+	len2 := len(t)
+	if len(t) > len(s) {
+		return 0
+	}
+	dp := make([][]int, len1+1)
+	for i := 0; i <= len1; i++ {
+		dp[i] = make([]int, len2+1)
+	}
+	for i := 0; i <= len1; i++ {
+		dp[i][0] = 1
+	}
+	for i := 1; i <= len2; i++ {
+		dp[0][i] = 0
+	}
+	for i := 1; i <= len1; i++ {
+		for j := 1; j <= len2; j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+	return dp[len1][len2] % (1e9 + 7)
+}
--- category/dp/dp_test.go
+++ category/dp/dp_test.go
@@ -720,3 +720,25 @@ func Test_isSubsequence(t *testing.T) {
 		})
 	}
 }
+
+func Test_numDistinct(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{"rabbbit", "rabbit"}, 3},
+		{"test2", args{"babgbag", "bag"}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numDistinct(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("numDistinct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
