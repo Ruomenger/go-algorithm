2a0808cdee313e885386faf7c669e81a3a76fc4f
ruomeng0x <ruomenger@foxmail.com>
2024-11-09 16:25:43 +0800

    add: 动态规划 1143. 最长公共子序列 medium

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -557,3 +557,25 @@ func findLength(nums1 []int, nums2 []int) int {
 	}
 	return maxLen
 }
+
+// longestCommonSubsequence 1143. 最长公共子序列 https://leetcode.cn/problems/longest-common-subsequence/description/
+func longestCommonSubsequence(text1 string, text2 string) int {
+	len1 := len(text1)
+	len2 := len(text2)
+	dp := make([][]int, len1+1)
+	for i := 0; i <= len1; i++ {
+		dp[i] = make([]int, len2+1)
+	}
+	maxLen := 0
+	for i := 1; i <= len1; i++ {
+		for j := 1; j <= len2; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+			maxLen = max(maxLen, dp[i][j])
+		}
+	}
+	return maxLen
+}
