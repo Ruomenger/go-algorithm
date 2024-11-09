6c0a0d7aba693dde45d2366e6180d0999438b19e
ruomeng0x <ruomenger@foxmail.com>
2024-11-09 14:07:54 +0800

    add: 动态规划 714. 买卖股票的最佳时机含手续费 medium

--- category/dp/dp.go
+++ category/dp/dp.go
@@ -486,3 +486,21 @@ func maxProfit5(prices []int) int {
 	}
 	return max(dp[len(prices)-1][3], dp[len(prices)-1][2], dp[len(prices)-1][0])
 }
+
+// maxProfit6 714. 买卖股票的最佳时机含手续费 https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+func maxProfit6(prices []int, fee int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][0] = -prices[0]
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]-fee)
+	}
+
+	return dp[len(prices)-1][1]
+}
